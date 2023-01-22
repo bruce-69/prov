0124ad9a936afcdcedebe7f6eb407db805724e1a
jorge <eduardcrow555@gmail.com>
2023-01-21 22:03:09 -0300

    ufa

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {  
+let [lista, setLista] = useState ([""])
+let [novoItem, setNovoItem] = useState("")
+  
+useEffect(() => {
+  setLista (["tarefa1"])
+},
+ [])
+
+return(
+  <>
+  <input value={novoItem} onChange={value => setNovoItem (value.target.value)} type="text"/>
+  <button onClick={() =>adicionarNovoItem ()}>Adicionar</button>
+<ul> 
+  {lista.map((item, index) =>(
+     <li>{item}
+     <button onClick={() => deletarItem(index)}>Deletar</button>
+     </li>
+  ))}
+</ul>
+</>
+)
+
+function adicionarNovoItem (){
+setLista([...lista , novoItem])
+setNovoItem("")
+}
+
+function deletarItem (index){
+  let temporario = [...lista]
+temporario.splice(index , 1)
+setLista(temporario)
+ }
+}
+
+
+
+
+
+export default App;
